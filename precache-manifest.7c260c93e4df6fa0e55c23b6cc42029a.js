ba916b49abc8b68fbacdcef0e210e2f38be8c820
Sarah Cooney <cooneys@usc.edu>
2020-08-04 18:33:24 -0700

    Updates

---
+++ precache-manifest.7c260c93e4df6fa0e55c23b6cc42029a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a87f413e50b7de4190940770a18bba6",
+    "url": "/streetsmart/index.html"
+  },
+  {
+    "revision": "1dcbe5d7804e29d3ace3",
+    "url": "/streetsmart/static/js/2.c39ec661.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/streetsmart/static/js/2.c39ec661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac1936a09008825cbb7",
+    "url": "/streetsmart/static/js/main.4c378823.chunk.js"
+  },
+  {
+    "revision": "6626b5cbd4d94f9badad",
+    "url": "/streetsmart/static/js/runtime-main.8572c110.js"
+  }
+]);
